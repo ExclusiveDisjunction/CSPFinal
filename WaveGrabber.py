63e5c623eb3f1c1a0234679c1815ce2a77fbe4a4
ExDisjunction <exdisj@outlook.com>
2023-11-06 16:31:00 -0500

    Started on Wave Grabber, got most code written. There is an isssue with non .wav files.

---
+++ WaveGrabber.py
@@ -0,0 +1,56 @@
+from pathlib import Path
+from pydub import AudioSegment
+import os
+import tkinter as tk
+from tkinter import messagebox
+from tkinter import filedialog
+
+# This file will prompt the user for a file, and convert it to a .wav and then return it.
+
+def GrabWaveFile() -> AudioSegment:
+    FilePath = PromptFile()
+    AudFile = GrabAudioSegment(FilePath)
+
+    if (AudFile == None):
+        messagebox.showinfo("The file requested could not be opened.")
+        return None
+
+    if (FilePath.suffix != ".wav"):
+        MsgResult = messagebox.askquestion("File not .wav", "The provided file is not .wav, and it will be converted. Is this ok? A copy of the file with an extension of .wav will be created.")
+        if (MsgResult != "Yes"):
+            return None
+
+        NewPath = os.path.basename(str(FilePath)) + FilePath.stem + ".wav"
+        AudFile.export(str(NewPath), format="wav")
+
+        AudFile = GrabAudioSegment(str(NewPath))
+        FilePath = NewPath
+
+    return AudFile
+    
+
+def GrabAudioSegment(FilePath: Path) -> AudioSegment:
+    Suffix = FilePath.suffix
+    Suffix = Suffix.removeprefix(".")
+    
+    file = open(FilePath)
+    file.close()
+
+    # This code is audioist. It wont open anything other than .wav :(
+
+    try:
+        AudioSeg = AudioSegment.from_file(str(FilePath), format=Suffix)
+    except FileNotFoundError as e:
+        messagebox.showerror("Error:", f"There was the following error: \n\"{str(e)}\"")
+        return None
+
+    return AudioSeg
+
+# We will have to first import a file.
+def PromptFile() -> Path:
+    FilePathRaw = filedialog.askopenfilename(filetypes=[("Audio Files", "*.mp3;*.wav;*.wma;*.aac")])
+    return Path(FilePathRaw)
+
+
+if __name__ == "__main__":
+    GrabWaveFile()
